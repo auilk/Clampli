e51deed10a1052b7dd66e738fae78c31f51f16aa
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-17 15:29:47 +0100

    Refactor code formatting for readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { NavBar } from "./components/NavBar";
-import NavLink from "./components/NavLink";
-import GitIcon from "./assets/icons/github-icon.png";
-import LinkedInIcon from "./assets/icons/linkedin-icon.png";
-import ClampliLogo from "./assets/logos/clampli-logo.png";
-import DottedBG from "./components/DottedBG";
-import UnitSelector from "./components/UnitSelector";
-import Form from "./components/Form";
 import ClampFormat from "./components/ClampFormat";
-
+import ClampResult from "./components/ClampResult";
 
 function App()
 {
   return (
     <div className="flex flex-col h-dvh">
-      {/* <NavBar gap={10}>
-        <NavLink position="right" link="https://github.com/auilk"text="GitHub" fontSize={0.6} icon={GitIcon}></NavLink>
-        <NavLink position="right" link="https://www.linkedin.com/in/abdelghafour-lbaz/" text="LinkedIn" fontSize={0.6} icon={LinkedInIcon}></NavLink>
-        <p position="center" className="text-white text-lg">Fluid type & layout—no math, just magic</p>
-        <div position="left" className="w-15 mr-1.5 h-10 scale-160 bg-cover bg-center bg-no-repeat"
-          style={{
-            backgroundImage: `url(${ClampliLogo})`,
-          }}></div>
-      </NavBar>
-      <DottedBG></DottedBG> */}
-      {/* <UnitSelector></UnitSelector> */}
-      {/* <Form></Form> */}
+      <ClampResult></ClampResult>
       <ClampFormat></ClampFormat>
     </div>
   );
