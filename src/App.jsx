2639bcddd14f456efb56063b569505a6fd30b6e8
auilk <abdelghafour.lbaz01@gmail.com>
2025-05-18 19:44:20 +0100

    Complete main page by adding and styling all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,67 @@
+import { NavBar } from "./components/NavBar";
+import NavLink from "./components/NavLink";
+import DottedBG from "./components/DottedBG";
+import Form from "./components/Form"; 
+
+import GitIcon from "./assets/icons/github-icon.png"
+import LinkedInIcon from "./assets/icons/linkedin-icon.png"
+import ClampliLogo from "./assets/logos/clampli-logo.png"
 import ClampResult from "./components/ClampResult";
+
 
 function App()
 {
   return (
     <div className="flex flex-col h-dvh">
-      <ClampResult></ClampResult>
+      <NavBar gap={10}>
+        <NavLink 
+          position="right" 
+          link="https://github.com/auilk" 
+          text="GitHub" 
+          fontSize={0.6} 
+          icon={GitIcon}
+        ></NavLink>
+
+        <NavLink 
+          position="right" 
+          link="https://www.linkedin.com/in/abdelghafour-lbaz/" 
+          text="LinkedIn" 
+          fontSize={0.6} 
+          icon={LinkedInIcon}
+        ></NavLink>
+
+        <p 
+          className="text-white text-lg" 
+          position="center"
+        >
+          Fluid type & layout—no math, just magic
+        </p>
+
+        <div 
+          className="w-15 mr-1.5 h-10 scale-160 bg-cover bg-center bg-no-repeat"
+          position="left"
+          style={{
+            backgroundImage: `url(${ClampliLogo})`,
+          }}
+        ></div>
+      </NavBar>
+
+      <div className="p-5 flex-1">
+        <DottedBG className="h-full flex justify-center">
+          <div className="w-full ml-80 mr-80 flex flex-col justify-center gap-10">
+            <div className="flex justify-between">
+              <Form></Form>
+
+              <Form></Form>
+            </div>
+
+            <div className="flex justify-center">
+              <ClampResult></ClampResult>
+            </div>
+          </div>
+        </DottedBG>
+      </div>
+
     </div>
   );
 }
